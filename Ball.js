f1f1b641b10a103c4e22f2aa73dfd4cec8642193
SankalpUGMan <sankalp_saoji@rediffmail.com>
2017-06-20 15:44:06 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var rotationSpeed = 100;
+var jumpHeight = 10;
+
+private var isFalling = false;
+
+function Update () 
+{
+	var rotation : float = Input.GetAxis ("Horizontal") * rotationSpeed;	
+	rotation *= Time.deltaTime;
+	GetComponent.<Rigidbody>().AddRelativeTorque (Vector3.back * rotation);
+
+	if (Input.GetKeyDown(KeyCode.M) && isFalling == false)
+	{
+		GetComponent.<Rigidbody>().velocity.y = jumpHeight;
+	}
+	isFalling = true;
+}
+function OnCollisionStay ()
+{
+	isFalling = false;
+}
---
+++ Camera.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var Target : Transform;
+var distance = -1.5;
+var lift = 5.5;
+
+function Update () 
+{
+	transform.position = Target.position + Vector3(0, lift, distance);
+	transform.LookAt (Target); 
+}
